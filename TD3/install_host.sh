ddf1297ae66d18022410a7ff427a683bff00fe5e
basilelt <blt_git@pm.me>
2024-10-16 13:34:05 +0200

    TD3

--- TD3/install_host.sh
+++ TD3/install_host.sh
@@ -9,32 +9,32 @@ function cleanup() {
 if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
     trap cleanup EXIT
 
-    sudo apt update
-    for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt remove $pkg; done
+    sudo apt-get update
+    for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
-    wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+    wget -O- https://apt-get.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt-get.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt-get/sources.list.d/hashicorp.list
 
-    sudo apt install -y fasttrack-archive-keyring
-    echo "deb https://fasttrack.debian.net/debian-fasttrack/ bookworm-fasttrack main contrib" | sudo tee /etc/apt/sources.list.d/fasttrack.list
-    echo "deb https://fasttrack.debian.net/debian-fasttrack/ bookworm-backports-staging main contrib" | sudo tee /etc/apt/sources.list.d/backports.list
+    sudo apt-get install -y fasttrack-archive-keyring
+    echo "deb https://fasttrack.debian.net/debian-fasttrack/ bookworm-fasttrack main contrib" | sudo tee /etc/apt-get/sources.list.d/fasttrack.list
+    echo "deb https://fasttrack.debian.net/debian-fasttrack/ bookworm-backports-staging main contrib" | sudo tee /etc/apt-get/sources.list.d/backports.list
     
-    sudo apt install -y ca-certificates \
+    sudo apt-get install -y ca-certificates \
                     curl \
                     gnupg
 
-    sudo install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    sudo install -m 0755 -d /etc/apt-get/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt-get/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt-get/keyrings/docker.gpg
 
     echo \
-        "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+        "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt-get/keyrings/docker.gpg] https://download.docker.com/linux/debian \
         "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo tee /etc/apt-get/sources.list.d/docker.list > /dev/null
         
-    sudo apt update
+    sudo apt-get update
 
-    sudo apt install -y docker-ce \
+    sudo apt-get install -y docker-ce \
                 docker-ce-cli \
                 containerd.io \
                 docker-buildx-plugin \
@@ -47,10 +47,10 @@ if [[ "${BASH_SOURCE[0]}" = "$0" ]]; then
     sudo systemctl start docker
 
     wget https://desktop.docker.com/linux/main/amd64/docker-desktop-4.22.0-amd64.deb
-    sudo apt install ./docker-desktop-4.22.0-amd64.deb
+    sudo apt-get install ./docker-desktop-4.22.0-amd64.deb
     rm docker-desktop-4.22.0-amd64.deb
 
     sudo usermod -aG docker $USER
 
-    sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y && sudo apt clean
+    sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y && sudo apt-get clean
 fi
--- TD3/install_vm.sh
+++ TD3/install_vm.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
+
 # Update the package index and install required dependencies
-sudo apt update
-sudo apt install -y ca-certificates curl gnupg
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
 
 # Add Docker's official GPG key
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo install -m 0755 -d /etc/apt-get/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt-get/keyrings/docker.gpg
+sudo chmod a+r /etc/apt-get/keyrings/docker.gpg
 
 # Set up the Docker repository
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt-get/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo tee /etc/apt-get/sources.list.d/docker.list > /dev/null
 
 # Update the package index (again)
-sudo apt update
+sudo apt-get update
 
 # Install Docker Engine, containerd, and Docker Compose
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Add the current user to the docker group to run Docker without sudo
 sudo usermod -aG docker $USER
